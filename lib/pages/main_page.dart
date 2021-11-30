7dfe16718c10fb1406569763e68cdcfa39c23223
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-30 10:37:27 +0900

    Main Page UI | Part 1

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,15 +1,54 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MainPage extends ConsumerWidget {
+  double? _deviceHeight;
+  double? _deviceWidth;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
     return Scaffold(
-      body: Center(),
+      backgroundColor: Colors.black,
+      body: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _backgroundWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundWidget() {
+    return Container(
+      height: _deviceHeight,
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: DecorationImage(
+          image: NetworkImage('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yACIAqAkSLkX4coHafpyLWAtQjw.jpg'),
+          fit: BoxFit.cover
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.2),
+          ),
+        ),
+      ),
     );
   }
 }
